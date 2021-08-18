bdcc19d17064dc49d09f14e8d95baa4c33d89afb
Israel Abraham <israelvictory87@gmail.com>
2021-08-18 11:36:57 +0100

    Created runtime

--- hng/settings.py
+++ hng/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STAIC_ROOT = "static"
+STATIC_ROOT = "static"
 STATICFILES_DIRS = [
     BASE_DIR / "resume/static"
 ]
