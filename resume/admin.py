634e6843d768e367aa2d0a5ba99bee1bc1d4ad33
Israel Abraham <israelvictory87@gmail.com>
2021-08-18 12:08:09 +0100

    Registered Message model to admin

--- resume/admin.py
+++ resume/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from resume.models import User, Project, Tag
+from resume.models import User, Project, Tag, Message
 
 
 admin.site.register(User)
 admin.site.register(Project)
 admin.site.register(Tag)
+admin.site.register(Message)
--- resume/models.py
+++ resume/models.py
@@ -28,3 +28,13 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
