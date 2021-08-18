c24472627ac623ebb972eb4f673fb3cfc668da7d
Israel Abraham <israelvictory87@gmail.com>
2021-08-18 10:27:26 +0100

    Created models

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    profile_picture = models.ImageField(upload_to="images/")
+    linkedin = models.URLField()
+    facebook = models.URLField()
+    twitter = models.URLField()
+    instagram = models.URLField()
+    github = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.tag
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="images/")
+    demo = models.URLField()
+    tag = models.ManyToManyField("Tag", related_name="tags")
+
+    def __str__(self):
+        return self.title
