c74c513151d3243d0a487d7be1f44df63b6a1192
Israel Abraham <israelvictory87@gmail.com>
2021-08-18 10:27:36 +0100

    Created views

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Project, User
+
+# Email Configuration
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.core.mail import EmailMessage
+
+
+def home(request):
+    projects = Project.objects.all()
+    user = User.objects.all()
+    context = {
+        'projects': projects,
+        'user': user,
+    }
+    return render(request, 'projects/home.html', context)
+
+
+# def sendEmail(request):
+
+#     if request.method == 'POST':
+#         template = render_to_string('projects/email_template.html', {
+#             'name': request.POST['name'],
+#             'email': request.POST['email'],
+#             'subject': request.POST['subject'],
+#             'message': request.POST['message'],
+#         })
+
+#         email = EmailMessage(
+#             request.POST['subject'],
+#             template,
+#             settings.EMAIL_HOST_USER,
+#             ['israelvictory87@gmail.com']
+#         )
+
+#         email.fail_silently = False
+#         email.send()
+#     return render(request, 'projects/email_sent.html')
